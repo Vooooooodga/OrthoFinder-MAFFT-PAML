7a389ce7b86f8823b5fa61421e4b145ea6335d26
Vooooooodga <bluis_gree@outlook.com>
2025-05-21 18:57:17 +0900

    脚本5改为使用slrum的版本

--- 5.run_MAFFT_for_aa_msa.sh
+++ 5.run_MAFFT_for_aa_msa.sh
@@ -1,61 +1,33 @@
 #!/bin/bash
-
-#$ -S /bin/bash
-
-#$ -cwd
-
-#$ -pe def_slot 64
-
-#$ -l s_vmem=20G
-
-#$ -l mem_req=20G
-
-  
+#SBATCH --job-name=mafft
+#SBATCH --output=mafft_%j.out
+#SBATCH --error=mafft_%j.err
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
 
 ulimit -s unlimited
-
 echo "Running on $(hostname)"
-
 echo "Starting at $(date)"
 
-  
-
 # 输入和输出目录定义
-
 input="./translated_proteins_deg"
-
 output="./aligned_translated_proteins_deg"
 
-  
-
 # 确保输出目录存在
-
 mkdir -p $output
 
-  
-
 # 检查输入文件
-
 echo "Checking input files in $input:"
-
 ls $input
 
-  
-
 # 为目录中的每个文件运行MAFFT
-
 for file in $input/*.fa; do
-
 echo "Processing $file..."
-
 outfile="$output/$(basename "${file%.fa}")_aligned.fa"
-
 echo "Output will be saved to $outfile"
-
-singularity exec /usr/local/biotools/m/mafft:7.525--h031d066_0 mafft --auto --thread 64 "$file" > "$outfile"
-
+singularity exec /usr/local/biotools/m/mafft:7.525--h031d066_0 mafft --auto --thread 16 "$file" > "$outfile"
 done
-
-  
 
 echo "Ending at $(date)"
